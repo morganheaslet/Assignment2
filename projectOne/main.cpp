1fe9812936f6e21b78bb47e94ab7a2fd48443ad1
Tony Comito <tony.comito@csu.fullerton.edu>
2018-09-24 18:00:24 -0700

    Initial Commit

---
+++ projectOne/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  projectOne
+//
+//  Created by Tony Comito on 9/24/18.
+//  Copyright © 2018 Tony Comito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
