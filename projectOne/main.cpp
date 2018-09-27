1e478023dd25a540b0824f99f2de222903471086
Tony Comito <tony.comito@csu.fullerton.edu>
2018-09-26 18:25:53 -0700

    Initial Upload
    Initial Commit.
    Currently the program will successfully tokenize the source file. We now need to work on identifying what each token is, but as instructed by Prof. Choi we must ensure we're using a method mirroring an FSM that is to be created.

--- projectOne/main.cpp
+++ projectOne/main.cpp
@@ -1,15 +1,60 @@
-//
-//  main.cpp
-//  projectOne
-//
-//  Created by Tony Comito on 9/24/18.
-//  Copyright © 2018 Tony Comito. All rights reserved.
-//
+/*
+  main.cpp
+  projectOne
+  CPSC323
+
+*/
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//For FSM use
+char letters[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+                    'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+                    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+                    'X', 'Y', 'Z'},
+    digits[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+int main() {
+    cout << "Please enter the source file name: ";
+    string sourceName;
+    cin >> sourceName;
+    fstream fileStream;
+    fileStream.open(sourceName + ".txt");
+    if(fileStream.is_open())
+    {
+        cout << "File Opened Successfully.\n";
+    }else{
+        cout << "File not found.\n";
+        return 0;
+    }
+    
+    
+    string line;
+    while (getline(fileStream, line))
+    {
+        stringstream linestream(line);
+        string token_str;
+        while (getline(linestream, token_str, ' '))
+        {
+            // token_cstr now has the cstring version of the token
+            char * token_cstr = new char[token_str.length()];
+            strcpy (token_cstr, token_str.c_str());
+            
+            char * p = strtok(token_cstr,"\t\r\n ");
+            while (p!=0)
+            {
+                cout << p << '\n';
+                p = strtok(NULL,"\t\r\n ");
+            }
+            delete[] token_cstr;
+            
+            //cout <<  token << endl;
+        }
+    }
+    fileStream.close();
     return 0;
 }
